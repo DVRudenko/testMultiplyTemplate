11455068964bf4847f82b2f24c12572621619eb3
Dmitry Rudenko <51669429+DVRudenko@users.noreply.github.com>
2021-11-05 22:34:53 +0300

    Add files via upload

--- miscMultipleTemplates.js
+++ miscMultipleTemplates.js
@@ -7,10 +7,20 @@ export default class MiscMultipleTemplates extends LightningElement {
   templateOne = true;
 
   render() {
+    console.log('render');
     return this.templateOne ? templateOne : templateTwo;
   }
 
+  renderedCallback() {
+    console.log('renderedCallback');
+  }
+
   switchTemplate(){
+    console.log('Button pressed');
     this.templateOne = this.templateOne === true ? false : true;
+  }
+
+  connectedCallback() {
+    console.log('connectedCallback');
   }
 }
